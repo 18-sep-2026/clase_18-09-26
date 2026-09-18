c6b97366265286aeedd478e3020edca2568cd29a
ValeMolina612 <1225100357.mvmr@gmail.com>
2026-09-18 14:26:20 -0600

    arreglos

---
+++ mx/edu/utng/edatos/RepasoArreglo.java
@@ -0,0 +1,21 @@
+package mx.edu.utng.edatos;
+public class RepasoArreglos {
+
+    public static void main(String[] args) {
+
+        System.out.println("Manejo de arreglos");
+
+        String[] nombres = new String[10];
+                int edades[] = {20, 21, 22, 23};
+
+                System.out.println("Tamaño " + nombres.length);
+                System.out.println("Edades " + edades.length);
+
+
+                //Indices
+                nombres[0] = "Juan Alberto";
+                //nombres[11] = "Juan Felipe"
+
+
+    }
+}
