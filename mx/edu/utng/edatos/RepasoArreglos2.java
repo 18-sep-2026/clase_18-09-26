b68e21d8ff54b447552ca8986811bfd86bc343fb
MOONMG4 <moonb9517@gmail.com>
2026-09-18 20:23:09 -0600

    Repaso Arreglos

---
+++ mx/edu/utng/edatos/RepasoArreglos2.java
@@ -0,0 +1,25 @@
+package mx.edu.utng.edatos;
+
+public class RepasoArreglos2 {
+
+    public static void main(String[] args) {
+        String[][] nombres = new String[7][3];
+
+        nombres[0][0] = "Jonathan Venancio";
+        nombres[0][1] = "Aguilar Hernandez";
+        nombres[0][2] = "18";
+
+        nombres[1][0] = "JOSE EMMANUEL";
+        nombres[1][1] = "ARREDONDO ESCALANTE";
+        nombres[1][2] = "22";
+
+        nombres[2][0] = "YANELI";
+        nombres[2][1] = "SANCHEZ CARDENAS";
+        nombres[2][2] = "19";
+
+        System.out.println("Apellidos registrados");
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i][1]);
+        }
+    }
+}
